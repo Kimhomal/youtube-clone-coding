441b1454d301138d5faf5617b2df3600b291787d
DESKTOP-H2CGUM0\hochu <hochul1005@gmail.com>
2021-05-10 11:36:53 +0900

    youtube video list

--- src/app.jsx
+++ src/app.jsx
@@ -1,8 +1,33 @@
+import React from 'react';
+import { useEffect, useState } from 'react';
 import './app.css';
+import Layout from './components/layout';
 
 function App() {
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    // component mount or state update event handler
+    console.log('useEffect');
+
+    const requestOptions = {
+      method: 'GET',
+      redirect: 'follow',
+    };
+
+    fetch(
+      'https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=10&key=AIzaSyB6WW1_Fmz97FylhWX_Wu1fmMjuEptFhno',
+      requestOptions
+    )
+      .then((response) => response.json())
+      .then((result) => setVideos(result.items))
+      .catch((error) => console.log('error', error));
+  }, []);
+
   return (
-    <h1>hello :)</h1>
+    <React.StrictMode>
+      <Layout videos={videos} />
+    </React.StrictMode>
   );
 }
 
---
+++ src/components/navbar.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { AppBar, IconButton, Toolbar, Typography } from '@material-ui/core';
+import MenuIcon from '@material-ui/icons/Menu';
+import styles from '../css/navbar.module.css';
+
+const Navbar = (props) => (
+  <AppBar position="fixed" className={styles['navbar-color']}>
+    <Toolbar>
+      <IconButton edge="start" color="inherit" aria-label="open drawer">
+        <MenuIcon></MenuIcon>
+      </IconButton>
+      <Typography noWrap>YOUTUBE</Typography>
+    </Toolbar>
+  </AppBar>
+);
+
+export default Navbar;
